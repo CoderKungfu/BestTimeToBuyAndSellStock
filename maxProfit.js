aef9d701eafbdcb5d419dab96150c3e92f197d8e
Michael Cheng <michael_cheng@tech.gov.sg>
2020-04-18 19:00:10 +0800

    First pass

--- maxProfit.js
+++ maxProfit.js
@@ -1,5 +1,36 @@
+const profitFromPrice = (lowestPrice, prices) => {
+  const lowestPricePostion = prices.indexOf(lowestPrice)
+  const highestPrice = prices.slice(lowestPricePostion+1).sort((a, b) => a - b).slice(-1)[0]
+
+  if (typeof highestPrice !== 'number') return 0
+  const profit = highestPrice - lowestPrice
+
+  return profit > 0 ? profit : 0
+}
+
 const maxProfit = (prices) => {
-    return 0
+  if (prices.length < 2) return 0
+
+  if (prices.length === 2) {
+    const profit = prices[1] - prices[0]
+    return profit > 0 ? profit : 0
+  }
+  
+  const sortedPrices = [...prices].sort((a, b) => a - b)
+
+  let maxProfitCandidate = null
+  for(let i=0; i<sortedPrices.length; i++) {
+    let candidateLowestPrice = sortedPrices[i]
+    let profit = profitFromPrice(candidateLowestPrice, prices)
+
+    if (maxProfitCandidate && maxProfitCandidate > profit) {
+      break
+    }
+
+    maxProfitCandidate = profit
+  }
+
+  return maxProfitCandidate
 }
 
 export default maxProfit
