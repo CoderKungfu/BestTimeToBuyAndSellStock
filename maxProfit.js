28f263fba58780f6f9a2bc867c1e02695cebcbae
Michael Cheng <michael_cheng@tech.gov.sg>
2020-04-19 11:10:20 +0800

    Second sitting with brute force approach.

--- maxProfit.js
+++ maxProfit.js
@@ -1,34 +1,15 @@
-const profitFromPrice = (lowestPrice, prices) => {
-  const lowestPricePostion = prices.indexOf(lowestPrice)
-  const highestPrice = prices.slice(lowestPricePostion+1).sort((a, b) => a - b).slice(-1)[0]
-
-  if (typeof highestPrice !== 'number') return 0
-  const profit = highestPrice - lowestPrice
-
-  return profit > 0 ? profit : 0
-}
-
 const maxProfit = (prices) => {
-  if (prices.length < 2) return 0
-
-  if (prices.length === 2) {
-    const profit = prices[1] - prices[0]
-    return profit > 0 ? profit : 0
-  }
-  
-  const sortedPrices = [...prices].sort((a, b) => a - b)
-
-  const profits = [0]
-  for(let i=0; i < sortedPrices.length; i++) {
-    let candidateLowestPrice = sortedPrices[i]
-    let profit = profitFromPrice(candidateLowestPrice, prices)
-
-    if (profit > 0) {
-      profits.push(profit)
+  let maxProfit = 0
+  for (let i = 0; i < prices.length; i++) {
+    const price = prices[i]
+    for (let j = i+1; j < prices.length; j++) {
+      const maxPrice = prices[j]
+      if (maxPrice > price && (maxPrice - price) > maxProfit) {
+        maxProfit = maxPrice - price
+      }
     }
   }
-
-  return profits.sort((a, b) => a - b)[profits.length - 1]
+  return maxProfit
 }
 
 export default maxProfit
